249378bfd00b3eec5689eb900efb6d10ddb04ff9
DeSouzaSR <sandro.fisica@gmail.com>
2024-08-28 18:07:03 -0300

    Criei arquivos para banco de dados

---
+++ db/CreateDatabase.sql
@@ -0,0 +1,20 @@
+USE Pycodebr;
+
+CREATE TABLE carros {
+    id INTEGER NOT NULL auto_increment,
+    marca VARCHAR(100),
+    modelo VARCHAR(100),
+    ano INTEGER,
+    PRIMARY KEY (id)
+};
+
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+SET character_set_results = utf8;
+SET collation_connection = utf8_general_ci;
+
+INSERT INTO carros(marca, modelo, ano) VALUES ('Fiat', 'Marea', 1999);
+INSERT INTO carros(marca, modelo, ano) VALUES ('Fiat', 'Uno', 1992);
+INSERT INTO carros(marca, modelo, ano) VALUES ('Ford', 'Escort', 1985);
+INSERT INTO carros(marca, modelo, ano) VALUES ('Chevrolet', 'Chevette', 1978);
+INSERT INTO carros(marca, modelo, ano) VALUES ('Volkswagen', 'Fusca', 1962);
