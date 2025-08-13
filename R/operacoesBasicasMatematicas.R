c46175aeee1392c64841bec3a13a5f7e9934ceda
DeSouzaSR <sandro.fisica@gmail.com>
2025-08-13 07:16:07 -0300

    Conclui aula de Vetores e Fatores

---
+++ R/operacoesBasicasMatematicas.R
@@ -0,0 +1,18 @@
+# Operações básicas
+6 + 3
+6 - 3
+6 * 3
+6 / 3
+6 ^ 3
+6 ** 3
+sqrt(81)
+81 ^ (1/2)
+216 ^ (1/3)
+10 / 3
+10 %% 3
+log(8)
+
+# Solução da atividade proposta
+(6 + 3 * 5) / 7 
+((9 * 4) ^ 2) ^ (1/3) # 1a forma
+(9 * 4) ^ (2/3) # 2a forma
